0cec861ecc3b447456bd44214391971383339d93
CHETNA <56215495+chetnaSharma24@users.noreply.github.com>
2019-10-10 23:16:48 +0530

    chetna_mca_06
    pasacals triangle

---
+++ pascal.cpp
@@ -0,0 +1,17 @@
+//pascals pyramid
+
+#include<stdio.h>
+int main()
+{ int m,n;
+printf("enter number of rows");
+scanf("%d",&m);
+for(int i=0;i<m;i++)
+{
+	for(int j=0;j<m;j++)
+	{
+		printf("%d",j);
+	}
+	printf("\n");
+	}	
+	return 0;printf("\n");
+}
